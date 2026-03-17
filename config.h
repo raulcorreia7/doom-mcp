e2b9d8653b202cf177dd76efb6e749724204e191
Raúl Correia <raulcorreia7@users.noreply.github.com>
2026-03-17 19:16:24 +0100

    fix(build): add RPATH + LTO optimizations for libdmcp

---
+++ config.h
@@ -0,0 +1,14 @@
+#define PACKAGE_NAME "Crispy Doom"
+#define PACKAGE_TARNAME "crispy-doom"
+#define PACKAGE_VERSION "7.1.0"
+#define PACKAGE_STRING "Crispy Doom 7.1.0"
+#define PROGRAM_PREFIX "crispy-"
+
+#define HAVE_FLUIDSYNTH
+#define HAVE_LIBSAMPLERATE
+#define HAVE_LIBPNG
+#define HAVE_DIRENT_H
+#define HAVE_DECL_STRCASECMP 1
+#define HAVE_DECL_STRNCASECMP 1
+
+/* #undef CRISPY_TRUECOLOR */
